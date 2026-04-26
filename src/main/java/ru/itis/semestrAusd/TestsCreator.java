ed1ec607f0d65b1d1155979bbb73f4bfe8c07490
MatveevaElena72 <126193836+MatveevaElena72@users.noreply.github.com>
2026-04-26 20:40:45 +0500

    Добавила код в класс TestsCreator

--- src/main/java/ru/itis/semestrAusd/TestsCreator.java
+++ src/main/java/ru/itis/semestrAusd/TestsCreator.java
@@ -1,4 +1,29 @@
 package ru.itis.semestrAusd;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
 public class TestsCreator {
+    private final Random generator= new Random();
+
+    public void generate(){
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < 100; i++){
+            int size = generator.nextInt(100, 10001); //размер ArrayList от 100 - 10000
+            ArrayList<Integer> arr = new ArrayList<>(size);
+            for(int j = 0; j < size; j++){
+                arr.add(generator.nextInt(0,10001)); //элементы от 0 до 10000
+            }
+            sb.append(String.join(" ", arr.toString().replaceAll("[\\[\\]]", "").trim()));//trim() удаляет пробелы в начале и в конце строки
+            sb.append("\n");
+        }
+         try(BufferedWriter writer = new BufferedWriter(new FileWriter("tests.txt"))){
+             writer.write(sb.toString());
+         } catch (IOException e){
+             System.out.println("Ошибка в записи файла" + e.getMessage());
+         }
+    }
 }
