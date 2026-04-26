7a91333aecb49c6248c9930b1ac7616ddce00249
MatveevaElena72 <126193836+MatveevaElena72@users.noreply.github.com>
2026-04-26 18:22:28 +0500

    Добавила код в класс RadiSort

--- src/main/java/ru/itis/semestrAusd/RadixSort.java
+++ src/main/java/ru/itis/semestrAusd/RadixSort.java
@@ -1,4 +1,61 @@
 package ru.itis.semestrAusd;
 
 public class RadixSort {
+    public int iterations = 0;
+
+    public void radixSort(int[] array){
+        int digit = 0;
+        int max = 0;
+        for(int i = 0; i < array.length; i++){ //ищем максимальный элемент
+            digit = Math.max(digit, array[i]);
+            iterations++;
+        }
+
+        while(digit > 0){//считаем кол-во разрядов в макс элементе
+            iterations++;
+            digit = digit / 10;
+            max++;
+        }
+
+        for(int i = 0; i < max; i++){
+            iterations++;
+            execute(array, i);
+        }
+    }
+
+    public void execute(int[] array, int r){
+        int r10 = 1;
+        int l = array.length;
+        int[] countDigits = new int[10];
+        int[] answer = new int[l];
+        int[] executions = new int[10];
+        executions[0] = 0;
+
+        for(int i = 0; i < r; i++){
+            r10 *= 10;
+            iterations++;
+        }
+
+        for(int i = 0; i < r; i++){
+            countDigits[(array[i] / r10) % 10]++;
+            iterations++;
+        }
+
+        for(int i = 0; i < 10; i++){
+            executions[i] = executions[i - 1] + countDigits[i - 1];
+            iterations++;
+        }
+
+        for(int i = 0; i < l; i++){
+            int x = (array[i] / r10) % 10;
+            answer[executions[x]] = array[i];
+            executions[x]++;
+            iterations++;
+        }
+
+        for(int i = 0; i < l; i++){
+            array[i] = answer[i];
+            iterations++;
+        }
+    }
 }
