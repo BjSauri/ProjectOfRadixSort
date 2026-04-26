0f08f0f99259cce5b269ce286abb20666266e2ae
MatveevaElena72 <126193836+MatveevaElena72@users.noreply.github.com>
2026-04-26 21:23:30 +0500

    Добавила реализацию Radix Sort в класс Main

--- src/main/java/ru/itis/semestrAusd/Main.java
+++ src/main/java/ru/itis/semestrAusd/Main.java
@@ -1,4 +1,35 @@
 package ru.itis.semestrAusd;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
+    public static void main(String[] args){
+        RadixSort radixSort = new RadixSort();
+        TestsCreator testsCreator = new TestsCreator();
+
+        testsCreator.generate(); //генерируем тесты
+
+        //читаем файл" tests.txt" и сортируем массивы
+        try(BufferedReader reader = new BufferedReader(new FileReader("tests.txt"))){
+            String line;
+            while((line = reader.readLine()) != null){
+                String[] massive = line.trim().split(" ");//преобразуем строку в массив
+                int[] arr = new int[massive.length];
+                for(int i = 0; i < massive.length; i++){
+                    arr[i] = Integer.parseInt(massive[i]);
+                }
+
+                long beginTime = System.nanoTime();
+                radixSort.radixSort(arr);
+                long endTime = System.nanoTime();
+                System.out.println("Количество элементов " + arr.length + ", время работы " + (endTime - beginTime) + ", количество итераций " + radixSort.iterations);
+
+                radixSort.iterations = 0; //обнуляем счетчик для следующего массива
+            }
+        } catch (IOException e){
+            System.out.println("Ошибка в прочтении файла " + e.getMessage());
+        }
+    }
 }
